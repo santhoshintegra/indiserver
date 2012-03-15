4f1d3024f3a2c06580352ec0c176624988ce5179
atuschen75@gmail.com <atuschen75@gmail.com@db375951-1bcc-9aab-8859-bb7e6102d985>
2012-03-15 17:52:37 +0000

    added lx200gps and lx200autostar drivers

---
+++ INDIserver/trunk/src/de/hallenbeck/indiserver/telescope_drivers/lx200autostar.java
@@ -0,0 +1,10 @@
+package de.hallenbeck.indiserver.telescope_drivers;
+
+/**
+ * Driver for Autostar-driven telescopes, derived from lx200generic
+ * @author atuschen
+ *
+ */
+public class lx200autostar extends lx200generic implements telescope_driver {
+
+}
