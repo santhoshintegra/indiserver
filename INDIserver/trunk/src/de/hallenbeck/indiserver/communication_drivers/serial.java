02eac7c36510e4f7ba7b2a0ce00123828afc0305
atuschen75@gmail.com <atuschen75@gmail.com@db375951-1bcc-9aab-8859-bb7e6102d985>
2012-03-15 17:46:34 +0000

    Added serial and usbhost_pl2303 drivers

---
+++ INDIserver/trunk/src/de/hallenbeck/indiserver/communication_drivers/serial.java
@@ -0,0 +1,51 @@
+package de.hallenbeck.indiserver.communication_drivers;
+
+/**
+ * Serial communication driver for devices with /dev/ttyS or /dev/ttyUSB support
+ * @author atuschen
+ *
+ */
+
+public class serial implements communication_driver {
+
+	public void setTimeout(int timeout) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public devicelist[] getDeviceList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int connect(String device) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int disconnect() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int sendCommand(String command) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void wait(int delay) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getAnswerInt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getAnswerString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
