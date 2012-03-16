2b066463d25c1fbc390006eb8fe71a48ef11ab5b
atuschen75 <atuschen75@gmail.com>
2012-03-16 17:04:35 +0000

    Renamed communication drivers

--- INDIserver/trunk/src/de/hallenbeck/indiserver/communication_drivers/bluetooth_serial.java
+++ INDIserver/trunk/src/de/hallenbeck/indiserver/communication_drivers/bluetooth_serial.java
@@ -1,24 +1,18 @@
 package de.hallenbeck.indiserver.communication_drivers;
 
 /**
- * PL2303 USB Serial Adapter Driver for
- * devices with android usbhost-support (3.2 upwards)
- * 
+ * Bluetooth-Serial-Port-Profile driver for short-range wireless connection to telescope
  * @author atuschen
  *
  */
-public class usbhost_pl2303 extends serial implements communication_driver_interface {
 
-	public void setTimeout(int timeout) {
-		// TODO Auto-generated method stub
-
-	}
+public class bluetooth_serial extends serial implements communication_driver_interface {
 
 	public devicelist[] getDeviceList() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
 	public int connect(String device) {
 		// TODO Auto-generated method stub
 		return 0;
@@ -28,15 +22,15 @@ public class usbhost_pl2303 extends serial implements communication_driver_inter
 		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+	public void setTimeout(int timeout) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	public int sendCommand(String command) {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-
-	public void wait(int delay) {
-		// TODO Auto-generated method stub
-
 	}
 
 	public int getAnswerInt() {
@@ -48,5 +42,12 @@ public class usbhost_pl2303 extends serial implements communication_driver_inter
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	public void wait(int delay) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 
 }
