edf67d30aea17110c87e8344380be08c4fcb4ab1
atuschen75 <atuschen75@gmail.com>
2012-03-18 20:49:09 +0000

    some cleanup

--- INDIserver/trunk/src/de/hallenbeck/indiserver/device_drivers/debug_telescope.java
+++ INDIserver/trunk/src/de/hallenbeck/indiserver/device_drivers/debug_telescope.java
@@ -1,7 +1,8 @@
 package de.hallenbeck.indiserver.device_drivers;
 
+import de.hallenbeck.indiserver.communication_drivers.communication_driver_interface;
 import android.os.Handler;
-import android.util.Log;
+
 
 /**
  * Dummy driver for debugging purposes only
@@ -15,94 +16,7 @@ public class debug_telescope extends telescope implements device_driver_interfac
 		// TODO Auto-generated constructor stub
 	}
 
-	private final String TAG="Telescope-Driver";
 	
-	private int get_firmware_info() {
-		Log.d(TAG,"Get Firmware Info");
-		return 0;
-	}
-
-	private int get_site_coords(int site) {
-		Log.d(TAG,"Get coords for site "+site);
-		return 0;
-	}
-
-	private int get_current_position() {
-		Log.d(TAG,"Get current position");
-		return 0;
-	}
-
-	private int set_datetime(int datetime) {
-		Log.d(TAG,"Set date+time" +datetime);
-		return 0;
-	}
-
-	private int set_utc_offset(int offset) {
-		Log.d(TAG,"Set UTC offset" +offset);
-		return 0;
-	}
-
-	private int set_site_coords(int site, float longitude, float latitude,
-			String name) {
-		Log.d(TAG,"Set coords for site " +site);
-		return 0;
-	}
-
-	private int set_slew_speed(int speed) {
-		Log.d(TAG,"Set slew speed" +speed);
-		return 0;
-	}
-
-	private void cancel_all() {
-		Log.d(TAG,"Cancel all movements");
-
-	}
-
-	private void cancel_north() {
-		Log.d(TAG,"Cancel northwards movement");
-
-	}
-
-	private void cancel_east() {
-		Log.d(TAG,"Cancel eastwards movement");
-
-	}
-
-	private void cancel_south() {
-		Log.d(TAG,"Cancel southwards movement");
-
-	}
-
-	private void cancel_west() {
-		Log.d(TAG,"Cancel westwards movement");
-
-	}
-
-	private void move_north() {
-		Log.d(TAG,"Move north");
-
-	}
-
-	private void move_east() {
-		Log.d(TAG,"Move east");
-
-	}
-
-	private void move_south() {
-		Log.d(TAG,"Move south");
-
-	}
-
-	private void move_west() {
-		Log.d(TAG,"Move west");
-
-	}
-
-	private int move_to_target(int radec) {
-		Log.d(TAG,"Move to target");
-		return 0;
-	}
-
 	public void sendINDImsg(String xmlcommand) {
 		// TODO Auto-generated method stub
 		
@@ -114,6 +28,11 @@ public class debug_telescope extends telescope implements device_driver_interfac
 	}
 
 	public void set_msg_handler(Handler mHandler) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void set_communication_driver(communication_driver_interface driver) {
 		// TODO Auto-generated method stub
 		
 	}
