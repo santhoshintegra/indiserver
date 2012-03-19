f2e6f7ab958b873f6dc1435d07768445becb2e1a
atuschen75@gmail.com <atuschen75@gmail.com@db375951-1bcc-9aab-8859-bb7e6102d985>
2012-03-19 19:58:32 +0000

    Extended Comments

--- INDIserver/trunk/src/de/hallenbeck/indiserver/device_drivers/telescope.java
+++ INDIserver/trunk/src/de/hallenbeck/indiserver/device_drivers/telescope.java
@@ -12,8 +12,81 @@ import de.hallenbeck.indiserver.communication_drivers.communication_driver_inter
  */
 public abstract class telescope implements device_driver_interface {
 	
-	protected static communication_driver_interface com_driver=null;
-	protected static boolean connected=false;
+	protected communication_driver_interface com_driver=null;
+	protected boolean connected=false;
+	protected telescope_information information;
+	protected telescope_pointing pointing;
+	
+	protected class telescope_information {
+		private String Description;
+		private String Version;
+		private String DateTime;
+		/**
+		 * @return the description
+		 */
+		public String getDescription() {
+			return Description;
+		}
+		/**
+		 * @param description the description to set
+		 */
+		public void setDescription(String description) {
+			Description = description;
+		}
+		/**
+		 * @return the version
+		 */
+		public String getVersion() {
+			return Version;
+		}
+		/**
+		 * @param version the version to set
+		 */
+		public void setVersion(String version) {
+			Version = version;
+		}
+		/**
+		 * @return the dateTime
+		 */
+		public String getDateTime() {
+			return DateTime;
+		}
+		/**
+		 * @param dateTime the dateTime to set
+		 */
+		public void setDateTime(String dateTime) {
+			DateTime = dateTime;
+		}
+	}
+	
+	protected class telescope_pointing {
+		private float RA;
+		private float DEC;
+		/**
+		 * @return the rA
+		 */
+		public float getRA() {
+			return RA;
+		}
+		/**
+		 * @param rA the rA to set
+		 */
+		public void setRA(float rA) {
+			RA = rA;
+		}
+		/**
+		 * @return the dEC
+		 */
+		public float getDEC() {
+			return DEC;
+		}
+		/**
+		 * @param dEC the dEC to set
+		 */
+		public void setDEC(float dEC) {
+			DEC = dEC;
+		}
+	}
 
 	/**
 	 * Set the driver for communication with the telescope
