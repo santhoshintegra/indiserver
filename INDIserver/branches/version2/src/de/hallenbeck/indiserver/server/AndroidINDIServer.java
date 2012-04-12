abdf72848ee4c0f51b5f2715a0665f4e4d8050eb
atuschen75@gmail.com <atuschen75@gmail.com@db375951-1bcc-9aab-8859-bb7e6102d985>
2012-04-12 10:26:27 +0000

    new classes AndroidINDIServer and INDIAndroidDevice

--- INDIserver/branches/version2/src/de/hallenbeck/indiserver/server/AndroidINDIServer.java
+++ INDIserver/branches/version2/src/de/hallenbeck/indiserver/server/AndroidINDIServer.java
@@ -1,8 +1,15 @@
 package de.hallenbeck.indiserver.server;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import de.hallenbeck.indiserver.R;
+import de.hallenbeck.indiserver.activities.main;
 import de.hallenbeck.indiserver.device_drivers.lx200autostar;
 import de.hallenbeck.indiserver.device_drivers.lx200basic;
 
@@ -11,14 +18,14 @@ import laazotea.indi.server.DefaultINDIServer;
 
 public class AndroidINDIServer extends DefaultINDIServer {
 
-	
+	private Context AppContext;
 	/**
 	 * Just loads the available driver.
 	 */
 	public AndroidINDIServer(Context context) {
 		super();
-
-		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(context);
+		AppContext = context;
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(AppContext);
 		String DeviceDriver = settings.getString("device_driver", null);
 		String ComDriver = settings.getString("com_driver", null);
 		String Device = settings.getString("device", null);
@@ -27,9 +34,10 @@ public class AndroidINDIServer extends DefaultINDIServer {
 		try {
 			loadAndroidDriver(lx200autostar.class, ComDriver, Device);
 			loadAndroidDriver(lx200basic.class, ComDriver, Device);
+			notifyUser("INDIserver started","Waiting for Clients...",true);
 		} catch (INDIException e) {
 			e.printStackTrace();
-			//notifyUser("INDIserver", "ERROR loading drivers", false);
+			notifyUser("INDIserver", "ERROR loading drivers", false);
 		}
 	}
 	
@@ -37,12 +45,36 @@ public class AndroidINDIServer extends DefaultINDIServer {
 		super.stopServer();
 		destroyJavaDriver(lx200autostar.class);
 		destroyJavaDriver(lx200basic.class);
+		notifyUser("INDIServer stopped", "All Clients disconnected", false);
 	}
 	
 	protected synchronized void loadAndroidDriver(Class cls, String ComDriver, String Device) throws INDIException {
 	    INDIAndroidDevice newDevice = new INDIAndroidDevice(this, cls, "class+-+" + cls.getName(), ComDriver, Device);
 	    addDevice(newDevice);
 	  }
+	
+	/**
+	 * Notify user about running server and connected clients
+	 */
+	public synchronized void notifyUser(String title, String message, boolean ongoing) {
+		
+		PendingIntent intent = PendingIntent.getActivity(AppContext, 0, new Intent(AppContext,main.class) , 0);
+		
+		String ns = Context.NOTIFICATION_SERVICE;
+		NotificationManager mNotificationManager = (NotificationManager) AppContext.getSystemService(ns);
+
+		NotificationCompat.Builder notificationbuilder = new NotificationCompat.Builder(AppContext);
+		
+		notificationbuilder.setContentTitle(title);
+		notificationbuilder.setContentText(message);
+		notificationbuilder.setTicker(message);
+		notificationbuilder.setSmallIcon(R.drawable.ic_launcher);
+		notificationbuilder.setContentIntent(intent);
+		notificationbuilder.setOngoing(ongoing);
+		Notification notification = notificationbuilder.getNotification();
+		mNotificationManager.notify(1, notification); 
+
+	}
 	  @Override
 	  protected void onClientConnected(String address) {
 	  }
