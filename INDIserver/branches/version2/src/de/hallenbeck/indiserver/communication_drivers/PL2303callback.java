ea0e31600da2e4eabc7be1a29335de91d63ce665
atuschen75 <atuschen75@gmail.com>
2012-04-19 21:20:19 +0000

    Cleanup PL2303 driver

--- INDIserver/branches/version2/src/de/hallenbeck/indiserver/communication_drivers/PL2303callback.java
+++ INDIserver/branches/version2/src/de/hallenbeck/indiserver/communication_drivers/PL2303callback.java
@@ -4,6 +4,7 @@ package de.hallenbeck.indiserver.communication_drivers;
 // Callback Interface for pl2303 driver
 // called by the driver after permission to access the device was granted by the user
 
-public interface pl2303connect {
-	public void onConnect();
+public interface PL2303callback {
+	public void pl2303_ConnectSuccess();
+	public void pl2303_ConnectFailed(String reason);
 }
